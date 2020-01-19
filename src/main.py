3e07605badd9ea64369505fa95a8fa512bd07675
Edward Boiko <Emboiko@gmail.com>
2020-01-18 19:34:35 -0500

    Socket Singleton is now a submodule

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
 from tkinter import Tk
 from CopyToMoveTo import CopyToMoveTo
-from socket_singleton import Socket_Singleton
+from Socket_Singleton.Socket_Singleton import Socket_Singleton
 
 
-def arg_handler(app, gui):
-    gui.list_box_from.insert("end", app.arguments[0])
-    app.arguments.pop()
+def arg_handler(args, gui):
+    gui.list_box_from.insert("end", args[0])
+    args.pop()
 
 
 def main():
@@ -17,6 +17,7 @@ def main():
     app.arguments.clear()
     app.trace(arg_handler, gui)
     root.mainloop()
+
 
 if __name__ == "__main__":
     main()
